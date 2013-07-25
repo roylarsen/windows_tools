a452f6e4b15a1c4e55f96b39fb7d0c41f60f20fa
Bak3y <baker.matt.b@gmail.com>
2013-07-25 12:41:15 -0500

    Update parse_winup.ps1
    Move $hostname var and add write-host for reboot required.

--- parse_winup.ps1
+++ parse_winup.ps1
@@ -2,7 +2,7 @@
 #todo: Remote machines (kind of the point)
 #todo: Present output in a more usable fashion
 wuauclt /DetectNow
-if(Test-Path -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"){Write-Output "reboot required!"}
-$updates = Get-Content C:\Windows\WindowsUpdate.log | Where-Object {$_ -like "* updates detected"}
 $hostname = hostname
+if(Test-Path -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"){Write-Output "Reboot required on $hostname!"}
+$updates = Get-Content C:\Windows\WindowsUpdate.log | Where-Object {$_ -like "* updates detected"}
 if($updates[-1].Split("`t")[5].Split(" ")[3] -ne 0){Write-Host "Updates needed on $hostname!"}
