ab7c49df36aa1dc4cfb41cf7b967ab6d8a34a96c
roylarsen <nonades@gmail.com>
2013-07-25 11:31:13 -0400

    added todos to parse_winup.ps1

--- parse_winup.ps1
+++ parse_winup.ps1
@@ -1,5 +1,6 @@
 ﻿# Brought to you by 24oz cans of delicious union made beverages
-
+#todo: Remote machines (kind of the point)
+#todo: Present output in a more usable fashion
 wuauclt /DetectNow
 if(Test-Path -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"){Write-Output "reboot required!"}
 $updates = Get-Content C:\Windows\WindowsUpdate.log | Where-Object {$_ -like "* updates detected"}
