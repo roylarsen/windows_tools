f8940470e6cdb9a700aca48c9ef5ceda936e5047
roylarsen <nonades@gmail.com>
2013-07-25 10:55:15 -0400

    Added parse_winup.ps1
    Script to check status of Windows Update on machine. Checks if it needs
    a reboot or if there are Updates to Install. Super basic atm.

---
+++ parse_winup.ps1
@@ -0,0 +1,7 @@
+﻿# Brought to you by 24oz cans of delicious union made beverages
+
+wuauclt /DetectNow
+if(Test-Path -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"){Write-Output "reboot required!"}
+$updates = Get-Content C:\Windows\WindowsUpdate.log | Where-Object {$_ -like "* updates detected"}
+
+if($updates[-1].Split("`t")[5].Split(" ")[3] -ne 0){Write-Output "updates needed!"}
