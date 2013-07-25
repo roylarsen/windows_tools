147524d0c10ff701e45332dca4a0229ce7c351fd
roylarsen <nonades@gmail.com>
2013-07-25 11:08:08 -0400

    Added usergroups.ps1
    Script to dump all the groups for all the users in a targeted OU
    
    Change this line as appropriate:
    $users = Get-ADUser -Filter "*" -searchBase <path to OU of users>
    -Properties *

---
+++ usergroups.ps1
@@ -0,0 +1,17 @@
+﻿param(
+    [string] $dept
+)
+Import-Module ActiveDirectory
+
+$out = "$dept users.txt"
+$users = Get-ADUser -Filter "*" -searchBase <path to OU of users> -Properties *
+
+foreach($user in $users){
+    $user.samaccountname | Out-File $out -append
+    foreach($groups in $user.MemberOf){
+        foreach($group in $groups){
+            ((($group.split(","))[0]).split("="))[1] | Out-File $out -Append
+        }
+    }
+    "`r`n" | Out-File $out -append
+}
