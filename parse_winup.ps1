9801d012fbb5d2355fc3c9e597eb90a554a4dad3
Bak3y <baker.matt.b@gmail.com>
2013-07-25 12:22:24 -0500

    Update parse_winup.ps1
    Added $hostname var to prep for running this command on remote machines

--- parse_winup.ps1
+++ parse_winup.ps1
@@ -4,5 +4,5 @@
 wuauclt /DetectNow
 if(Test-Path -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"){Write-Output "reboot required!"}
 $updates = Get-Content C:\Windows\WindowsUpdate.log | Where-Object {$_ -like "* updates detected"}
-
-if($updates[-1].Split("`t")[5].Split(" ")[3] -ne 0){Write-Output "updates needed!"}
+$hostname = hostname
+if($updates[-1].Split("`t")[5].Split(" ")[3] -ne 0){Write-Host "Updates needed on " $hostname"!"}
